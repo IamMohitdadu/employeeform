fd31aefe1ced4c0d2d5eea99993e6d3237f726e1
Mohit Dadu <Mohit Dadu>
2017-01-11 19:31:45 +0530

    added updating table and bootstrap

--- js/validate.js
+++ js/validate.js
@@ -2,99 +2,85 @@
 function ValidateForm( event ) 
 {	
 	event.preventDefault();
-
-// validation for employee name
-	if (document.EmployeeRegistration.Empname.value == " ") 
-	{ 
+	// validation for employee name
+	if (document.EmployeeRegistration.Empname.value == ""){ 
 		alert('Enter the Employee Name.'); 
 		document.EmployeeRegistration.Empname.focus(); 
 		return false; 
 	}
 		
 	// validation for employee Id
-	if (document.EmployeeRegistration.Empid.value == " ") 
-	{ 
+	if (document.EmployeeRegistration.Empid.value == ""){ 
 		alert('Enter the Employee ID.'); 
 		document.EmployeeRegistration.Empid.focus(); 
 		return false; 
 	}
 	
 	// validation for validation for district
-	if (document.EmployeeRegistration.District.value == -1) 
-	{ 
+	if (document.EmployeeRegistration.District.value == -1){ 
 		alert('Select District.'); 
 		document.EmployeeRegistration.District.focus(); 
 		return false; 
 	}
 		
 	// validation for state
-	if (document.EmployeeRegistration.State.value == -1) 
-	{ 
+	if (document.EmployeeRegistration.State.value == -1){ 
 		alert('Select State.'); 
 		document.EmployeeRegistration.State.focus(); 
 		return false; 
 	}
 		
 	// validation for Pincode
-	if (document.EmployeeRegistration.Pincode.value == " ")
-	{
+	if (document.EmployeeRegistration.Pincode.value == " "){
 		alert('Enter Pincode.');
 		document.EmployeeRegistration.Pincode.focus();
 		return false;
 	}
-	if ((isNaN(document.EmployeeRegistration.Pincode.value)) || (document.EmployeeRegistration.Pincode.value.length != 6))
-	{
+	if ((isNaN(document.EmployeeRegistration.Pincode.value)) || (document.EmployeeRegistration.Pincode.value.length != 6)){
 		alert('Enter Correct Pincode.');
 		document.EmployeeRegistration.Pincode.focus();
 		return false;			
 	}	
 		
 	// validation for gender
-	if (( EmployeeRegistration.sex[0].checked == false) && (EmployeeRegistration.sex[1].checked == false))
-	{
+	if (( EmployeeRegistration.sex[0].checked == false) && (EmployeeRegistration.sex[1].checked == false)){
 		alert('Select Gender(Male or Female)');
 		return false;
 	}
 	
 	// validation for emailId
-	if (EmployeeRegistration.emailid.value == "") 
-	{ 
+	if (EmployeeRegistration.emailid.value == "") { 
 	alert('Email address is required.'); 
 	EmployeeRegistration.emailid.focus(); 
 	return false; 
 	}
-	if (EmployeeRegistration.emailid.value.indexOf("@") < 1 || EmployeeRegistration.emailid.value.indexOf(".") < 1) 
-	{ 	
+	if (EmployeeRegistration.emailid.value.indexOf("@") < 1 || EmployeeRegistration.emailid.value.indexOf(".") < 1) { 	
 	alert('Please enter a valid email address.'); 
 	EmployeeRegistration.emailid.focus(); 
 	return false; 
 	}
 	
 	// validation for Mobile number
-	if (document.EmployeeRegistration.mobileno.value == " ")
-	{
+	if (document.EmployeeRegistration.mobileno.value == " "){
 		alert('Enter Mobile Number.');
 		document.EmployeeRegistration.mobileno.focus();
 		return false;
 	}
-	if ((isNaN(document.EmployeeRegistration.mobileno.value)) || (document.EmployeeRegistration.mobileno.value.length != 10))
-	{
+	if ((isNaN(document.EmployeeRegistration.mobileno.value)) || (document.EmployeeRegistration.mobileno.value.length != 10)){
 		alert('Enter Correct Mobile Number(do not add +91/0).');
 		document.EmployeeRegistration.mobileno.focus();
 		return false;			
 	}	
 	
 	// validation for Department
-	if (document.EmployeeRegistration.department.value == -1) 
-	{ 
+	if (document.EmployeeRegistration.department.value == -1) { 
 		alert('Select Department.'); 
 		document.EmployeeRegistration.department.focus(); 
 		return false; 
 	}
 	
 	// validation for district
-	if (document.EmployeeRegistration.position.value == -1) 
-	{ 
+	if (document.EmployeeRegistration.position.value == -1) { 
 		alert('Select Position.'); 
 		document.EmployeeRegistration.position.focus(); 
 		return false; 
@@ -148,6 +134,7 @@ function GenerateTable() {
 	//Editing and Deleting Data using Buttons
 	row.insertCell(15).innerHTML = '<input type="button" class="btn btn-success" id="Edit" value="Edit" onclick="editRow(this)" /><br/>';
 	row.insertCell(16).innerHTML = '<input type="button" class="btn btn-danger" id="Delete" value="Delete" onclick="deleteRow(this)" />';
+	row.insertCell(16).innerHTML = '<input type="button" class="btn btn-success" id="Save" value="Save" onclick="saveRow(this)" />';
 
 }
 
@@ -175,23 +162,39 @@ function editRow(r){
 	r14 = x[13].innerHTML;
 	r15 = x[14].innerHTML;
 	
-	x[0].innerHTML = '<input type = "text" id="disable" size=10 value="'+r1+'" />';
-	x[1].innerHTML = '<input type = "text" id="disable" size=10 value= "'+r2+'" />';
-	x[2].innerHTML = '<input type = "text" id="disable" size=10 value= "'+r3+'" />';
-	x[3].innerHTML = '<input type = "text" id="disable" size=10 value= "'+r4+'" />';
-	x[4].innerHTML = '<input type = "text" id="disable" size=10 value= "'+r5+'" />';
-	x[5].innerHTML = '<input type = "text" id="disable" size=10 value= "'+r6+'" />';
-	x[6].innerHTML = '<input type = "text" id="disable" size=10 value= "'+r7+'" />';
-	x[7].innerHTML = '<input type = "text" id="disable" size=10 value= "'+r8+'" />';
-	x[8].innerHTML = '<input type = "text" size=10 id="disable" value= "'+r9+'" />';
-	x[9].innerHTML = '<input type = "text" size=10 id="disable" value= "'+r10+'" />';
-	x[10].innerHTML = '<input type = "text" size=10 id="disable" value= "'+r11+'" />';
-	x[11].innerHTML = '<input type = "text" size=10 id="disable" value= "'+r12+'" />';
-	x[12].innerHTML = '<input type = "text" size=10 id="disable" value= "'+r13+'" />';
-	x[13].innerHTML = '<input type = "text" size=10 id="disable" value= "'+r14+'" />';
-	x[14].innerHTML = '<input type = "text" size=10 id="disable" value= "'+r15+'" />';
-	document.getElementById('disable').onblur = function() {
-    tmp = document.getElementById('disable').value;
-    x[0].innerHTML = tmp;
-  };
+	x[0].innerHTML = '<input type = "text" id="f1" size=6 value="'+r1+'" />';
+	x[1].innerHTML = '<input type = "text" id="f2" size=5 value= "'+r2+'" />';
+	x[2].innerHTML = '<input type = "text" id="f3" size=6 value= "'+r3+'" />';
+	x[3].innerHTML = '<input type = "text" id="f4" size=5 value= "'+r4+'" />';
+	x[4].innerHTML = '<input type = "text" id="f5" size=5 value= "'+r5+'" />';
+	x[5].innerHTML = '<input type = "text" id="f6" size=5 value= "'+r6+'" />';
+	x[6].innerHTML = '<input type = "text" id="f7" size=5 value= "'+r7+'" />';
+	x[7].innerHTML = '<input type = "text" id="f8" size=5 value= "'+r8+'" />';
+	x[8].innerHTML = '<input type = "text" id="f9" size=5 value= "'+r9+'" />';
+	x[9].innerHTML = '<input type = "text" id="f10" size=5  value= "'+r10+'" />';
+	x[10].innerHTML = '<input type = "text" id="f11" size=5 value= "'+r11+'" />';
+	x[11].innerHTML = '<input type = "text" id="f12" size=5 value= "'+r12+'" />';
+	x[12].innerHTML = '<input type = "text" id="f13" size=5 value= "'+r13+'" />';
+	x[13].innerHTML = '<input type = "text" id="f14" size=5 value= "'+r14+'" />';
+	x[14].innerHTML = '<input type = "text" id="f15" size=5 value= "'+r15+'" />';
+
+}
+
+function saveRow(obj){
+	var x=obj.parentNode.parentNode.childNodes;
+    x[0].innerHTML = document.getElementById("f1").value;
+	x[1].innerHTML = document.getElementById("f2").value;
+	x[2].innerHTML = document.getElementById("f3").value;
+	x[3].innerHTML = document.getElementById("f4").value;
+	x[4].innerHTML = document.getElementById("f5").value;
+	x[5].innerHTML = document.getElementById("f6").value;
+	x[6].innerHTML = document.getElementById("f7").value;
+	x[7].innerHTML = document.getElementById("f8").value;
+	x[8].innerHTML = document.getElementById("f9").value;
+    x[9].innerHTML = document.getElementById("f10").value;
+	x[10].innerHTML = document.getElementById("f11").value;
+	x[11].innerHTML = document.getElementById("f12").value;
+	x[12].innerHTML = document.getElementById("f13").value;
+	x[13].innerHTML = document.getElementById("f14").value;
+	x[14].innerHTML = document.getElementById("f15").value;
 }
